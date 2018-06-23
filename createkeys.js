10c129ba175c575fd863197bccd4d3bd0052d3cf
Steve Thomas <steve.thomas@nextsteplogistics.com>
2018-06-23 14:46:32 -0400

    initial commit

---
+++ createkeys.js
@@ -0,0 +1,38 @@
+//These are npm packages that are required to run the program
+//This will be clear in the next two classes
+var openpgp = require('openpgp');
+var fs = require('fs');
+
+
+if (process.argv[2] && process.argv[3] && process.argv[4] && process.argv[5]){
+    //This options object is to identify the keys and how to encrypt them
+    var options = {
+        userIds: [{ name: process.argv[2], email: 'practice@example.org' }],
+        numBits: 2048,
+        passphrase: process.argv[3]
+    };
+    
+    var pubKey, privKey;
+    
+    //This function generates the PGPkeys
+    openpgp.generateKey(options).then(function(key) {
+        privKey = key.privateKeyArmored;
+        pubKey = key.publicKeyArmored;
+        console.log('Key generated');
+        console.log(privKey);
+        console.log(pubKey);
+        //Generates the Private Key
+        fs.writeFile(process.argv[4]+'.txt', privKey, function(err){
+            if (err) throw err;
+        });
+        //Generates the Public Key
+        fs.writeFile(process.argv[5]+'.txt', pubKey, function(err){
+            if (err) throw err;
+        });
+    });
+} 
+else 
+{
+    console.log("no keys have been generated");
+}
+
---
+++ readme.md
@@ -0,0 +1,17 @@
+## Case Study - PGP Encryption
+
+### Warning - you do not need to know about PGP to understand.  The purpose of this exercise is to understand what is happening to the code at a high-level.
+
+You and a friend want to send messages over the internet, but you do not want to send them clear text because you are paranoid about people seeing your messages.
+
+So to combat this, you made a createkeys.js to encrypt and decrypt messages.  The only thing to know at this point is these ensure the security of messages.  
+
+This example requires you to know
+- how to run a node program
+- process.argv
+
+Take a look at the file and try to understand what is going on from a high level.  Note the if statement and what is required.  Try to run this code and observe the folder and any new files that may pop up.
+
+If done correctly, you should get
+
+node 
